27bf6f599447b5b045321f8db03f5f17759b7807
JamesPodesta <jamespodesta@kromestudios.com>
2022-10-31 03:11:33 +1000

    looks like win/linux are good now

--- createRelease.bat
+++ createRelease.bat
@@ -8,6 +8,7 @@ rem PC RELEASE
 mkdir Mad64Win_%build%
 xcopy ..\x64\release\mad64.exe Mad64Win_%build%\
 xcopy ..\readme.txt Mad64Win_%build%\
+xcopy ..\font.otf Mad64Win_%build%\
 xcopy ..\*.dll Mad64Win_%build%\
 xcopy ..\samples Mad64Win_%build%\samples\
 7z a Mad64Win_%build%.zip Mad64Win_%build%
@@ -17,6 +18,7 @@ rem LINUX RELEASE
 mkdir Mad64Linux_%build%
 xcopy E:\UbuntuShared\mad64 Mad64Linux_%build%\
 xcopy ..\readme.txt Mad64Linux_%build%\
+xcopy ..\font.otf Mad64Linux_%build%\
 xcopy ..\samples Mad64Linux_%build%\samples\
 7z a Mad64Linux_%build%.zip Mad64Linux_%build%
 
