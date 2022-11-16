d9fedc32f42002f2dd7a1bd05383c238c586cb87
JamesPodesta <jamespodesta@kromestudios.com>
2022-11-17 00:44:41 +1000

    no cp command on linux

--- mkbuild.sh
+++ mkbuild.sh
@@ -3,11 +3,11 @@ cmake --build build --config release
 read -p "Build version: " bld
 mkdir Mad64_V$fname
 bp=Mad64_V$fname
-copy build/Mad64 $bp
-copy build/font.ttf $bp
-copy build/font.otf $bp
-copy build/readme.txt $bp
-copy -r build/samples $bp/samples
+cp build/Mad64 $bp
+cp build/font.ttf $bp
+cp build/font.otf $bp
+cp build/readme.txt $bp
+cp -r build/samples $bp/samples
 tar -czvf Mad64_V${fname}.tar.gz $bp
 
 
