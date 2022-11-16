13ed0e2c888b87e611745ca0b7513694e8571c8b
JamesPodesta <jamespodesta@kromestudios.com>
2022-11-17 00:50:31 +1000

    updated build script

--- mkbuild.sh
+++ mkbuild.sh
@@ -1,13 +1,12 @@
 git pull
 cmake --build build --config release
 read -p "Build version: " bld
-mkdir Mad64_V$fname
-bp=Mad64_V$fname
+bp=Mad64_V$bld
+mkdir $bp
 cp build/Mad64 $bp
 cp build/font.ttf $bp
 cp build/font.otf $bp
 cp build/readme.txt $bp
 cp -r build/samples $bp/samples
-tar -czvf Mad64_V${fname}.tar.gz $bp
-
-
+tar -czvf ${bp}.tar.gz $bp
+rm -r $bp
