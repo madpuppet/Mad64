64d8b53a650cd7613d99c747dfd0acd9a1fa52fe
JamesPodesta <jamespodesta@kromestudios.com>
2022-11-17 01:17:11 +1000

    specific build scripts for ubuntu and debian

--- deb_mkbuild.sh
+++ deb_mkbuild.sh
@@ -1,13 +1,13 @@
 git pull
 cmake --build build --config release
 read -p "Build version: " bld
-bp=Mad64_V$bld
+bp=Mad64_DEB_V$bld
 mkdir $bp
 cp build/Mad64 $bp
-cp build/font.ttf $bp
-cp build/font.otf $bp
-cp build/readme.txt $bp
-cp -r build/samples $bp/samples
+cp font.ttf $bp
+cp font.otf $bp
+cp readme.txt $bp
+cp -r samples $bp/samples
 tar -czvf ${bp}.tar.gz $bp
 rm -r $bp
 
---
+++ ubu_mkbuild.sh
@@ -0,0 +1,13 @@
+git pull
+cmake --build build --config release
+read -p "Build version: " bld
+bp=Mad64_UBU_V$bld
+mkdir $bp
+cp build/Mad64 $bp
+cp font.ttf $bp
+cp font.otf $bp
+cp readme.txt $bp
+cp -r samples $bp/samples
+tar -czvf ${bp}.tar.gz $bp
+rm -r $bp
+
